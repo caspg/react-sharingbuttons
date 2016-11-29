29a00f93b061f17647c3e836ca5a4c80b099b982
Kacper Golinski <kacper.golinski@gmail.com>
2016-11-29 18:07:12 +0000

    install stuff and setup hello world

---
+++ dev/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+import HelloWorld from '../src'
+
+render(
+  <HelloWorld />,
+  document.getElementById('app'),
+)
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+const devPath = path.join(__dirname, 'dev')
+
+module.exports = {
+  entry: path.join(devPath, 'index.jsx'),
+  output: {
+    path: devPath,
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: devPath,
+  },
+  module: {
+    loaders: [
+      {
+        test: /.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
+}
