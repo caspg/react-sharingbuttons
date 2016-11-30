dfb8c4ce3f73b351e346fd170d449d0cb74fc1e3
Kacper Golinski <kacper.golinski@gmail.com>
2016-11-30 23:28:01 +0000

    export all buttons in src/index

--- dev/index.jsx
+++ dev/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
 
-import Email from '../src/buttons/Email'
-import Facebook from '../src/buttons/Facebook'
-import Google from '../src/buttons/Google'
-import Pinterest from '../src/buttons/Pinterest'
-import Reddit from '../src/buttons/Reddit'
-import Tumblr from '../src/buttons/Tumblr'
-import Twitter from '../src/buttons/Twitter'
+import {
+  Email,
+  Facebook,
+  Google,
+  Pinterest,
+  Reddit,
+  Tumblr,
+  Twitter,
+} from '../src'
 
 const Buttons = () =>
   <div>
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import React from 'react'
-
-export default () =>
-  <h1>Hello World</h1>
+export { default as Email } from './buttons/Email'
+export { default as Facebook } from './buttons/Facebook'
+export { default as Google } from './buttons/Google'
+export { default as Pinterest } from './buttons/Pinterest'
+export { default as Reddit } from './buttons/Reddit'
+export { default as Tumblr } from './buttons/Tumblr'
+export { default as Twitter } from './buttons/Twitter'
