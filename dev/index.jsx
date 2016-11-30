95812d80d6779e34ddba559cd4202f6e7401bccf
Kacper Golinski <kacper.golinski@gmail.com>
2016-11-30 23:21:01 +0000

    render all buttons at dev

--- dev/index.jsx
+++ dev/index.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import { render } from 'react-dom'
 
+import Email from '../src/buttons/Email'
 import Facebook from '../src/buttons/Facebook'
+import Google from '../src/buttons/Google'
+import Pinterest from '../src/buttons/Pinterest'
+import Reddit from '../src/buttons/Reddit'
+import Tumblr from '../src/buttons/Tumblr'
+import Twitter from '../src/buttons/Twitter'
+
+const Buttons = () =>
+  <div>
+    <Email />
+    <Facebook />
+    <Google />
+    <Pinterest />
+    <Reddit />
+    <Tumblr />
+    <Twitter />
+  </div>
 
 render(
-  <Facebook />,
+  <Buttons />,
   document.getElementById('app'),
 )
