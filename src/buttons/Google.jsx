99746d845e57ca1b2c8663d364bc291c0195d2ae
Dustin Graves <dust8806@gmail.com>
2019-01-16 11:21:52 -0800

    Added support for React 16 and updated tests

--- src/buttons/Google.jsx
+++ src/buttons/Google.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import SharingButton from '../components/SharingButton'
 import GoogleIcon from '../icons/Google'
 import encodeURI from '../utils/encodeURI'
--- src/buttons/Tumblr.jsx
+++ src/buttons/Tumblr.jsx
@@ -1,4 +1,5 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 import SharingButton from '../components/SharingButton'
 import TumblrIcon from '../icons/Tumblr'
 import encodeURI from '../utils/encodeURI'
